2a8fd2f40a44de70f2f607efbe59b21255011eb6
Nadalig Carlos <c.nadalig@itecriocuarto.org.ar>
2025-07-02 13:25:41 -0300

    first commit for master_work branch

--- codigo.js
+++ codigo.js
@@ -1,12 +1,10 @@
 // Get all the images
 image_array = [
-  'yurippe.jpeg', 'chise.png', 'emma.png', 'erika.png', 'eriri.jpg', 'haruhi.jpg', //6
-  'hinata.png', 'jibril.jpg', 'kyaru.png', 'megumin.jpg', 'midori.jpg', 'mimimi.jpg', //6
-  'nodoka.png', 'olivia.png', 'rebecca.jpg', 'rem.jpg', 'rumia.jpg', 'sinon.jpg', //6
-  'tatsumaki.jpg', 'tsukimi.png', 'weiss.jpg', 'yui.jpeg', 'yume.jpg', 'nowa.png', //6
-  'serina.png', 'mieruko.png', 'kani.png', 'alice.png', 'futaba.png', 'hayasaka.png', //6
-  'konami.png', 'marija.png', 'subaru.png', 'ume.png', 'yumechi.png', //5
-]//35
+  'bercouli.png', 'fives.png', 'franky.webp', 'gauma.jpg', 'geese.png', 'himmel.webp', //6
+  'hinata.jpg', 'kanna.png', 'klein.jpeg', 'kotaro.jpg', 'lewis.jpeg', 'maki.jpg', //6
+  'medina.png', 'mumen_rider.jpg', 'qrow.jpg', 'ray.jpg', 'rengoku.png', 'sasha.webp', //6
+  'suwa.jpg', //1
+]//19
 
 function get_random_image(){
   // Get a random index
